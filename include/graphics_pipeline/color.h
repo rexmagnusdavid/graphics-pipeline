428cf3510ff95eeec952828d61e76a277488a23d
David Wang <rexmagnusdavid@gmail.com>
2025-10-09 08:05:18 -0400

    feat: Added more colors

--- include/graphics_pipeline/color.h
+++ include/graphics_pipeline/color.h
@@ -6,6 +6,9 @@ constexpr unsigned int RED = 0xFF0000FF;
 constexpr unsigned int GREEN = 0x00FF00FF;
 constexpr unsigned int BLUE = 0x0000FFFF;
 constexpr unsigned int WHITE = 0xFFFFFFFF;
+constexpr unsigned int YELLOW = 0xFFFF00FF;
+constexpr unsigned int MAGENTA = 0xFF00FFFF;
+constexpr unsigned int CYAN = 0x00FFFFFF;
 
 constexpr int RED_SHIFT = 24;
 constexpr int GREEN_SHIFT = 16;
